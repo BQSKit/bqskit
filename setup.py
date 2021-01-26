e1f39f4f6ecb903abee4482baddbc65b09c2cb64
Ethan Smith <ethan@ethanhs.me>
2021-01-26 03:29:33 -0800

    Move everything over to setup.cfg

--- setup.py
+++ setup.py
@@ -2,9 +2,5 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    project_urls={
-        'Bug Tracker': 'https://github.com/BQSKit/bqskit/issues',
-        'Source Code': 'https://github.com/BQSKit/bqskit',
-    },
     packages=find_packages(exclude=['tests*', 'examples*']),
 )
