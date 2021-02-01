92636c5e0ed33117f19bc68669dd0ce20ef4fb9a
Ed Younis <edyounis123@gmail.com>
2021-02-01 07:15:57 -0800

    UnitaryMatrix Class Start

---
+++ bqskit/qis/unitarymatrix.py
@@ -0,0 +1,26 @@
+"""
+This module implements the UnitaryMatrix class.
+
+This is a concrete unitary matrix that can be operated on.
+"""
+
+from typing import List
+from typing import Optional
+
+import numpy as np
+
+from bqskit.qis import Unitary
+
+
+class UnitaryMatrix (Unitary):
+    """The UnitaryMatrix Class."""
+
+    def __init__ ( self, utry: np.ndarray ):
+        """
+        Unitary Matrix Constructor
+        """
+        self.utry = utry
+
+    def get_unitary(self, params: Optional[List[float]] = None) -> np.ndarray:
+        return self.utry
+s
