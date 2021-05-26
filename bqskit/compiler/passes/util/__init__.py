149ef0ab323affb9a3ee737367832beabf9c0c2f
Ed Younis <edyounis123@gmail.com>
2021-05-26 13:12:55 -0700

    Implemented SetRandomSeedPass

--- bqskit/compiler/passes/util/__init__.py
+++ bqskit/compiler/passes/util/__init__.py
@@ -3,5 +3,6 @@ from __future__ import annotations
 
 from bqskit.compiler.passes.util.compress import CompressPass
 from bqskit.compiler.passes.util.inject import InjectDataPass
+from bqskit.compiler.passes.util.random import SetRandomSeedPass
 
-__all__ = ['CompressPass', 'InjectDataPass']
+__all__ = ['CompressPass', 'InjectDataPass', 'SetRandomSeedPass']
