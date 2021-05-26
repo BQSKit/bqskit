84105751003c189695255704344ef27622918130
Ed Younis <edyounis123@gmail.com>
2021-05-26 10:29:34 -0700

    Implemented NotPredicate

--- bqskit/compiler/passes/control/predicates/__init__.py
+++ bqskit/compiler/passes/control/predicates/__init__.py
@@ -2,5 +2,6 @@
 from __future__ import annotations
 
 from bqskit.compiler.passes.control.predicates.change import ChangePredicate
+from bqskit.compiler.passes.control.predicates.notpredicate import NotPredicate
 
-__all__ = ['ChangePredicate']
+__all__ = ['ChangePredicate', 'NotPredicate']
