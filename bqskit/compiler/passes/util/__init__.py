5e46a25dc184de39b52f4f5368d19d6eb9ce1b6c
Ed Younis <edyounis123@gmail.com>
2021-05-26 17:38:58 -0700

    Implemented RecordStatsPass

--- bqskit/compiler/passes/util/__init__.py
+++ bqskit/compiler/passes/util/__init__.py
@@ -4,6 +4,13 @@ from __future__ import annotations
 from bqskit.compiler.passes.util.compress import CompressPass
 from bqskit.compiler.passes.util.inject import InjectDataPass
 from bqskit.compiler.passes.util.random import SetRandomSeedPass
+from bqskit.compiler.passes.util.record import RecordStatsPass
 from bqskit.compiler.passes.util.unfold import UnfoldPass
 
-__all__ = ['CompressPass', 'InjectDataPass', 'SetRandomSeedPass', 'UnfoldPass']
+__all__ = [
+    'CompressPass',
+    'InjectDataPass',
+    'SetRandomSeedPass',
+    'RecordStatsPass',
+    'UnfoldPass',
+]
