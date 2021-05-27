2f3a7f2920268bafd5455fa8015b214ca2a51443
Ed Younis <edyounis123@gmail.com>
2021-05-26 21:06:08 -0700

    Region Scaffolding

--- bqskit/ir/region.py
+++ bqskit/ir/region.py
@@ -4,14 +4,18 @@ from __future__ import annotations
 from typing import NamedTuple
 
 from bqskit.ir.location import CircuitLocation
+from bqskit.ir.point import CircuitPoint
 
 
 class CircuitRegion(NamedTuple):
     """
     The CircuitRegion class.
 
-    A CircuitRegion is an ordered set of qudit indices. In context, this usually
-    describes where a gate or operation is being applied.
+    A CircuitRegion is an contiguous area in a circuit.
     """
     qudits: CircuitLocation
     bounds: dict[int, tuple[int, int]]
+
+    def overlaps(self, other: CircuitPoint | CircuitRegion) -> bool:
+        """Return true if `other` overlaps this region."""
+        pass  # TODO
