d70ecd9b485bb71cb15de3221cc413cbf8d0c707
Ethan Smith <ethan@ethanhs.me>
2021-06-12 13:04:17 -0700

    Add residuals stuff

--- bqskit/ir/opt/cost/differentiable.py
+++ bqskit/ir/opt/cost/differentiable.py
@@ -7,6 +7,7 @@ from typing import Sequence
 import numpy as np
 
 from bqskit.ir.opt.cost.function import CostFunction
+from bqskit.ir.opt.cost.residual import ResidualsFunction
 
 
 class DifferentiableCostFunction(CostFunction):
@@ -32,3 +33,27 @@ class DifferentiableCostFunction(CostFunction):
     ) -> tuple[float, np.ndarray]:
         """Return the cost and gradient given the input parameters."""
         return self.get_cost(params), self.get_grad(params)
+
+class DifferentiableResidualsFunction(ResidualsFunction):
+    """
+    The DifferentiableResidualsFunction base class.
+
+    A DifferentiableResidualsFunction is a differentiable map from a vector of
+    real numbers to a matrix where the rows are the gradients for each input parameter.
+
+    A DifferentiableResidualsFunction exposes the `get_grad` abstract method
+    and the `get_residuals_and_grad` method. When subclassing, you only need to
+    implement the `gen_cost` and `gen_grad` function factories. You can
+    overwrite gen_cost_and_grad for optimization in some cases.
+    """
+
+    @abc.abstractmethod
+    def get_grad(self, params: Sequence[float] | np.ndarray) -> np.ndarray:
+        """Return the residuals gradient given the input parameters."""
+
+    def get_cost_and_grad(
+        self,
+        params: Sequence[float] | np.ndarray,
+    ) -> tuple[float, np.ndarray]:
+        """Return the residuals and gradient given the input parameters."""
+        return self.get_residuals(params), self.get_grad(params)
