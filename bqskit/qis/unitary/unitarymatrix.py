Fixed distance calculation to handle negatives

bqskit/qis/unitary/unitarymatrix.py
@@ -119,7 +119,8 @@ class UnitaryMatrix(Unitary, StateVectorMap):
         other = UnitaryMatrix(other)
         num = np.abs(np.trace(other.get_numpy().conj().T @ self.get_numpy()))
         dem = self.get_dim()
-        dist = np.sqrt(1 - ((num / dem) ** 2))
+        val = 1 - ((num / dem) ** 2)
+        dist = np.sign(val) * np.sqrt(np.abs(val))
         return dist if dist > 0.0 else 0.0
 
     def get_statevector(self, in_state: StateLike) -> StateVector: