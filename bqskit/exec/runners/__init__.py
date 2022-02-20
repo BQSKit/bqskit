70d6375344208a9834470f2abfe872f7191cadf0
Ethan Smith <ethan@ethanhs.me>
2022-02-19 19:55:49 -0800

    Make qiskit import optional

--- bqskit/exec/runners/__init__.py
+++ bqskit/exec/runners/__init__.py
@@ -1,8 +1,13 @@
 """This package contains several CircuitRunner implementations."""
 from __future__ import annotations
 
-from bqskit.exec.runners.ibmq import IBMQRunner
+__all__ = ['QuestRunner', 'SimulationRunner']
+
+try:
+    from bqskit.exec.runners.ibmq import IBMQRunner   # noqa
+    __all__.append('IBMQRunner')
+except ImportError:
+    pass
+
 from bqskit.exec.runners.quest import QuestRunner
 from bqskit.exec.runners.sim import SimulationRunner
-
-__all__ = ['QuestRunner', 'IBMQRunner', 'SimulationRunner']
