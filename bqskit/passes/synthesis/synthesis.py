b6728914648fd80b231cc9a8c22b1c9cd27581a9
Ed Younis <edyounis123@gmail.com>
2022-03-25 09:42:39 -0700

    Update

--- bqskit/passes/synthesis/synthesis.py
+++ bqskit/passes/synthesis/synthesis.py
@@ -87,23 +87,21 @@ class SynthesisPass(BasePass):
             multistarts = kwargs['multistarts']
             kwargs['multistarts'] = 1
 
-        futures: list[list[Future]] = []
 
-        client.scatter(circuits)
+        flat_futures = client.map(
+            Circuit.instantiate,
+            [c.copy() for c in circuits * multistarts],
+            target=target,
+            pure=False,
+            priority=10000,
+            **kwargs
+        )
 
-        for circuit in circuits:
-            futures.append([])
-            for i in range(multistarts):
-                futures[-1].append(
-                    client.submit(
-                        Circuit.instantiate,
-                        circuit,
-                        pure=False,
-                        target=target,
-                        **kwargs,
-                    ),
-                )
-
+        futures = [[] for _ in range(len(circuits))]
+        for i in range(len(circuits)):
+            for j in range(multistarts):
+                futures[i].append(flat_futures[i * len(circuits) + j])
+        
         return futures
 
     def gather_best_results(
