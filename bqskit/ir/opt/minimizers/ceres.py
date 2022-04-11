c04dc3cfe8686380e5bb0f76ed635b1c9aaf4bb1
Ethan Smith <ethan@ethanhs.me>
2022-04-11 14:04:08 -0700

    Change defaults for Ceres optimization

--- bqskit/ir/opt/minimizers/ceres.py
+++ bqskit/ir/opt/minimizers/ceres.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 import logging
 
 from bqskitrs import LeastSquaresMinimizerNative
+from typing_extensions import Self
 
 from bqskit.ir.opt.minimizer import Minimizer
 
@@ -17,3 +18,9 @@ class CeresMinimizer(LeastSquaresMinimizerNative, Minimizer):
     The CeresMinimizer attempts to instantiate the circuit such that the
     circuit's cost, given by a CostFunction, is minimized.
     """
+
+    def __new__(
+        cls: type[Self], num_threads: int = 1, ftol: float = 1e-6,
+        gtol: float = 1e-10, report: bool = False,
+    ) -> Self:
+        return super().__new__(cls, num_threads, ftol, gtol, report)
