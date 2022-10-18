1f7842a715012f4cfb618b0585a81d049816d24a
Alon Kukliansky <alonkukl@gmail.com>
2022-10-18 15:56:03 -0700

    Removing slowdownfactor as it is not used

--- bqskit/ir/opt/instantiaters/qfactor_batch.py
+++ bqskit/ir/opt/instantiaters/qfactor_batch.py
@@ -30,7 +30,6 @@ class QFactor_batch(Instantiater):
         dist_tol: float = 1e-10,
         max_iters: int = 100000,
         min_iters: int = 1000,
-        slowdown_factor: float = 0.0
     ):
 
         if not isinstance( diff_tol_a, float ) or diff_tol_a > 0.5:
@@ -48,16 +47,14 @@ class QFactor_batch(Instantiater):
         if not isinstance( min_iters, int ) or min_iters < 0:
             raise TypeError( "Invalid minimum number of iterations." )
 
-        if slowdown_factor < 0 or slowdown_factor >= 1:
-            raise TypeError( "Slowdown factor is a positive number less than 1." )
+        
 
         self.diff_tol_a = diff_tol_a
         self.diff_tol_r = diff_tol_r
         self.dist_tol   = dist_tol
         self.max_iters  = max_iters
         self.min_iters  = min_iters
-        self.slowdown_factor = slowdown_factor
-
+        
 
 
     def _initilize_circuit_tensor(
@@ -86,10 +83,12 @@ class QFactor_batch(Instantiater):
         """Instantiate `circuit`, see Instantiater for more info."""
 
 
+    params = np.array(x0)
+
     locations = [op.location for op in circuit]
     gates = [op.gate for op in circuit]
     
-    target_untry_builder = self._initilize_circuit_tensor(gates, locations, x0)
+    target_untry_builder = self._initilize_circuit_tensor(gates, locations, params)
 
 
     c1 = 0
@@ -111,13 +110,13 @@ class QFactor_batch(Instantiater):
                 break;
 
         # from right to left
-        param_index = len(x0)
+        param_index = len(params)
         for k in reversed(range(amount_of_gates)):
             gate = gates[k]
             location = locations[k]
             
             op = circuit[k]
-            gparams = x0[param_index - amount_of_params:param_index]
+            gparams = params[param_index - amount_of_params:param_index]
             
 
             # Remove current gate from right of circuit tensor
@@ -127,7 +126,7 @@ class QFactor_batch(Instantiater):
             if amount_of_params > 0:
                 env = ct.calc_env_matrix( location )            
                 new_params =  gate.optimize(env)
-                x0[param_index - amount_of_params:param_index] = new_params 
+                params[param_index - amount_of_params:param_index] = new_params 
                 gparams = new_params
 
             # Add updated gate to left of circuit tensor
@@ -143,7 +142,7 @@ class QFactor_batch(Instantiater):
             location = locations[k]
             
             op = circuit[k]
-            gparams = x0[param_index: param_index + amount_of_params]
+            gparams = params[param_index: param_index + amount_of_params]
             # Remove current gate from left of circuit tensor
             target_untry_builder.apply_left( gate.get_unitary(params = gparams), location, inverse = True, check_arguments = False)
 
@@ -151,7 +150,7 @@ class QFactor_batch(Instantiater):
             if gate.num_params > 0:
                 env = ct.calc_env_matrix( location )            
                 new_params =  gate.optimize(env)
-                x0[param_index : param_index + amount_of_params] = new_params 
+                params[param_index : param_index + amount_of_params] = new_params 
                 gparams = new_params
 
             # Add updated gate to right of circuit tensor
@@ -169,44 +168,7 @@ class QFactor_batch(Instantiater):
             logger.info( f"iteration: {it}, cost: {c1}" )
 
         if it % 40 == 0:
-            target_untry_builder = self._initilize_circuit_tensor(gates, locations, x0)
+            target_untry_builder = self._initilize_circuit_tensor(gates, locations, params)
 
-    
 
-    def _single_iteration(target, gates_location, gates_untrys):
-
-    circuit = [Gate(utry, loc) for utry,loc in zip(gates_untrys, gates_location)]
-    ct = CircuitTensor(target, circuit)
-    
-    for _ in range(40):
-        for k in range( len( circuit ) ):
-            rk = len( circuit ) - 1 - k
-
-            # Remove current gate from right of circuit tensor
-            ct.apply_right( rk, inverse = True )
-
-            # Update current gate
-            if not circuit[rk].fixed:
-                env = ct.calc_env_matrix( circuit[rk].location )
-                circuit[rk].update( env, self.slowdown_factor )
-
-            # Add updated gate to left of circuit tensor
-            ct.apply_left( rk )
-
-            # from left to right
-        for k in range( len( circuit ) ):
-            
-             # Remove current gate from left of circuit tensor
-            ct.apply_left( k, inverse = True )
-
-            # Update current gate
-            if not circuit[k].fixed:
-                env = ct.calc_env_matrix( circuit[k].location )
-                circuit[k].update( env, self.slowdown_factor )
-
-            # Add updated gate to right of circuit tensor
-            ct.apply_right(k)
-
-    c1 = jnp.abs( jnp.trace( ct.utry ) )
-    c1 = 1 - ( c1 / ( 2 ** ct.num_qubits ) )
-    return c1, jnp.array([g.utry for g in ct.gate_list])
+    return params
