0c9e6dca5ad16bf4974affacadfdc36959f6b2fb
Alon Kukliansky <alonkukl@gmail.com>
2022-10-25 14:14:58 -0700

    Adding batch with jit

--- bqskit/ir/opt/instantiaters/__init__.py
+++ bqskit/ir/opt/instantiaters/__init__.py
@@ -7,9 +7,10 @@ from bqskit.ir.opt.instantiaters.qfactor import QFactor
 from bqskit.ir.opt.instantiaters.qfactor_einsum import QFactor_einsum
 from bqskit.ir.opt.instantiaters.qfactor_jax import QFactor_jax
 from bqskit.ir.opt.instantiaters.qfactor_jax_batched import QFactor_jax_batched
+from bqskit.ir.opt.instantiaters.qfactor_jax_batched_jit import QFactor_jax_batched_jit
 
 
-instantiater_order = [QFactor, Minimization, QFactor_einsum, QFactor_jax, QFactor_jax_batched]
+instantiater_order = [QFactor, Minimization, QFactor_einsum, QFactor_jax, QFactor_jax_batched, QFactor_jax_batched_jit]
 
 
 __all__ = [
@@ -18,5 +19,6 @@ __all__ = [
     'QFactor_einsum',
     'QFactor_jax',
     'qfactor_jax_batched',
+    'qfactor_jax_batched_jit',
     'Minimization',
 ]
---
+++ bqskit/ir/opt/instantiaters/qfactor_jax_batched_jit.py
@@ -0,0 +1,241 @@
+from functools import partial
+import logging
+import time
+from typing import Any
+from typing import TYPE_CHECKING
+
+import numpy as np
+import numpy.typing as npt
+
+import jax
+import jax.numpy as jnp
+
+from bqskit.ir.opt.instantiaters.qfactor_jax import QFactor_jax
+from bqskit.qis.state.state import StateVector
+from bqskit.qis.unitary.unitarybuilder import UnitaryBuilder
+from bqskit.qis.unitary.unitarymatrix import UnitaryMatrix
+
+
+if TYPE_CHECKING:
+    from bqskit.ir.circuit import Circuit
+
+
+_logger = logging.getLogger(__name__)
+_logger.setLevel('DEBUG')
+
+
+class QFactor_jax_batched_jit(QFactor_jax):
+    """The QFactor batch circuit instantiater."""
+
+    def __init__(
+        self,
+        diff_tol_a: float = 1e-12 ,
+        diff_tol_r: float = 1e-6,
+        dist_tol: float = 1e-10,
+        max_iters: int = 100000,
+        min_iters: int = 1000,
+    ):
+
+        if not isinstance( diff_tol_a, float ) or diff_tol_a > 0.5:
+            raise TypeError( "Invalid absolute difference threshold." )
+
+        if not isinstance( diff_tol_r, float ) or diff_tol_r > 0.5:
+            raise TypeError( "Invalid relative difference threshold." )
+
+        if not isinstance( dist_tol, float ) or dist_tol > 0.5:
+            raise TypeError( "Invalid distance threshold." )
+
+        if not isinstance( max_iters, int ) or max_iters < 0:
+            raise TypeError( "Invalid maximum number of iterations." )
+
+        if not isinstance( min_iters, int ) or min_iters < 0:
+            raise TypeError( "Invalid minimum number of iterations." )
+
+        
+
+        self.diff_tol_a = diff_tol_a
+        self.diff_tol_r = diff_tol_r
+        self.dist_tol   = dist_tol
+        self.max_iters  = max_iters
+        self.min_iters  = min_iters
+        
+
+    def instantiate_multistart(
+        self,
+        circuit,#: Circuit,
+        target: UnitaryMatrix | StateVector,
+        starts: list[npt.NDArray[np.float64]]
+    ) :
+        """Instantiate `circuit`, see Instantiater for more info."""
+
+        amount_of_starts = len(starts)
+        starts = jnp.array(starts)      
+        locations = tuple([op.location for op in circuit])
+        gates = tuple([op.gate for op in circuit])
+        
+
+        untrys = [[] for _ in range(amount_of_starts)]
+        param_index = 0
+        for gate in gates:
+            amount_of_params_in_gate = gate.num_params
+            
+            for start_index in range(amount_of_starts):                
+                gparams = starts[start_index][param_index: param_index + amount_of_params_in_gate]
+                untrys[start_index].append(gate.get_unitary(params=gparams, check_params=False, use_jax=True).numpy)
+            
+            param_index += amount_of_params_in_gate
+
+        untrys = jnp.array(untrys)
+        n = 40
+        c1s = [0] * amount_of_starts
+        c2s = [1] * amount_of_starts
+        it = 0
+        best_start = 0
+
+        sweep_vmaped = jax.vmap(_sweep_circuit, in_axes = (None, None, None, 0, None))
+
+        while(True):
+            it += 1
+            # Termination conditions
+            if it*n > self.min_iters:
+
+                if all([jnp.abs(c1 - c2) <= self.diff_tol_a + self.diff_tol_r * jnp.abs( c1 ) for c1, c2 in zip(c1s,c2s)]):
+                    # diff = jnp.abs(c1 - c2)
+                    _logger.info( f"Terminated: |c1 - c2| = "
+                                " <= diff_tol_a + diff_tol_r * |c1|." )
+                    best_start = np.argmin(c1s)
+                    break
+
+                if it*n > self.max_iters:
+                    _logger.info( "Terminated: iteration limit reached." )
+                    best_start = jnp.argmin(c1s)
+                    break
+
+            c2s = c1s
+            
+            tic = time.perf_counter()
+            c1s, untrys = sweep_vmaped(target, locations, gates, untrys, n)
+
+            c1s = c1s.block_until_ready()
+            toc = time.perf_counter()
+
+            print(f"iteration took {toc-tic} seconeds")
+            
+
+            reached_desired_distance = [c1 <= self.dist_tol for c1 in c1s]
+            if any(reached_desired_distance):
+                
+                best_start = reached_desired_distance.index(True)
+                _logger.info( f"Terminated: {it} c1 = {c1s} <= dist_tol.\n Best start is {best_start}" )
+                break
+
+            if it % 4 == 0:
+                _logger.info( f"iteration: {it*n}, costs: {c1s}" )
+
+
+        params = []
+        for untry, gate in zip(untrys[best_start], gates):
+            params.extend(gate.get_params(untry))
+
+        return np.array(params)
+
+    @staticmethod
+    def get_method_name() -> str:
+        """Return the name of this method."""
+        return 'qfactor_jax_batched_jit'
+
+    @staticmethod
+    def can_internaly_perform_multistart() -> bool:
+        """Probes if the instantiater can internaly perform multistrat """
+        return True
+
+
+
+def _initilize_circuit_tensor(
+        target_num_qudits,
+        target_radixes,
+        locations,        
+        target_mat,
+        untrys
+    ):
+
+        use_jax = True
+        target_untry_builder = UnitaryBuilder(target_num_qudits, target_radixes, target_mat.conj().T)
+        
+        for loc, untry in zip(locations, untrys):
+            target_untry_builder.apply_right(untry, loc, check_arguments=False, use_jax=use_jax)
+            
+        return target_untry_builder
+
+__initilize_circuit_tensor_jit = jax.jit(_initilize_circuit_tensor, static_argnums=(0,1,2))
+    
+# __initilize_circuit_tensor_jit = _initilize_circuit_tensor
+
+
+def _single_sweep(locations, gates, untrys, amount_of_gates, target_untry_builder):
+    # from right to left
+    for k in reversed(range(amount_of_gates)):
+        gate = gates[k]
+        location = locations[k]
+        untry = untrys[k]
+
+            # Remove current gate from right of circuit tensor
+        target_untry_builder.apply_right(untry , location, inverse = True, check_arguments = False, use_jax=True)
+
+            # Update current gate
+        if gate.num_params > 0:
+            env = target_untry_builder.calc_env_matrix( location , use_jax=True)            
+            untry =  gate.optimize(env, get_untry=True, use_jax=True)
+            untrys[k] = untry
+                
+
+            # Add updated gate to left of circuit tensor
+        target_untry_builder.apply_left( untry, location,  check_arguments = False, use_jax=True)
+
+
+        # from left to right
+    for k in range(amount_of_gates):
+        gate = gates[k]
+        location = locations[k]
+        untry = untrys[k]
+            
+            # Remove current gate from left of circuit tensor
+        target_untry_builder.apply_left( untry, location, inverse = True, check_arguments = False, use_jax=True)
+
+            # Update current gate
+        if gate.num_params > 0:
+            env = target_untry_builder.calc_env_matrix(location, use_jax=True)            
+            untry =  gate.optimize(env, get_untry=True, use_jax=True)
+            untrys[k] = untry
+            
+            # Add updated gate to right of circuit tensor
+        target_untry_builder.apply_right( untry, location,  check_arguments = False, use_jax=True)
+    
+    return target_untry_builder
+
+_single_sweep_jit = jax.jit(_single_sweep, static_argnums=(0,1,3))
+# _single_sweep_jit = _single_sweep
+    
+def _sweep_circuit(target:UnitaryMatrix, locations, gates, untrys, n:int):
+
+    amount_of_gates = len(gates)
+    untrys_as_matrixs = []
+    for gate_index in range(amount_of_gates):
+        untrys_as_matrixs.append(UnitaryMatrix(untrys[gate_index], gates[gate_index].radixes, check_arguments=False, use_jax=True))
+
+    untrys = untrys_as_matrixs
+
+    target_untry_builder = __initilize_circuit_tensor_jit(target.num_qudits, target.radixes, locations, target.numpy, untrys)
+
+    amount_of_qudits = target.num_qudits
+
+
+    for _ in range(n):        
+        target_untry_builder = _single_sweep_jit(locations, gates, untrys, amount_of_gates, target_untry_builder)
+
+    c1 = jnp.abs( jnp.trace( jnp.array(target_untry_builder.get_unitary(use_jax=True).numpy) ) )
+    c1 = 1 - ( c1 / ( 2 ** amount_of_qudits ) )
+
+    return c1, jnp.array([untry.numpy for untry in untrys])
+
+
