5039ead32dc0ea2d30f8acb6557ec94dfc9e0c29
Ed Younis <edyounis123@gmail.com>
2022-10-27 12:24:00 -0700

    Added Doc Check Override

--- bqskit/utils/docs.py
+++ bqskit/utils/docs.py
@@ -7,6 +7,8 @@ import os
 def building_docs() -> bool:
     """Return true if currently building documentations."""
 
+    if 'BQSKIT_DOC_CHECK_OVERRIDE' in os.environ:
+        return False
     if 'READTHEDOCS' in os.environ:
         return True
     if '__SPHINX_BUILD__' in os.environ:
