b8f712d9d5e49fa6ba3d211f1b73424bdbd3bacd
Ed Younis <edyounis123@gmail.com>
2023-01-23 09:34:44 -0500

    Update

--- bqskit/utils/random.py
+++ bqskit/utils/random.py
@@ -9,7 +9,7 @@ import numpy as np
 def seed_random_sources(seed: int) -> None:
     """Seed the various sources of randomness BQSKit uses."""
     # set rand() seed, used by Ceres
-    libc = ctypes.CDLL(None)
+    libc = ctypes.CDLL(ctypes.util.find_library('c'))
     libc.srand(seed)
     # set numpy seed
     np.random.seed(seed)
