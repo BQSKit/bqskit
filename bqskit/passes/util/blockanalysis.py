72d8f8d25588829862d08a02c3c0467c7c2e9892
Mathias Weiden <mtweiden@berkeley.edu>
2023-02-07 18:11:25 -0800

    Added Callable annotations

--- bqskit/passes/util/blockanalysis.py
+++ bqskit/passes/util/blockanalysis.py
@@ -3,43 +3,46 @@ from __future__ import annotations
 
 import logging
 from typing import Any
+from typing import Callable
 
 from bqskit.compiler.basepass import BasePass
 from bqskit.ir.circuit import Circuit
-from bqskit.ir.gates import CircuitGate
 from bqskit.ir.gate import Gate
+from bqskit.ir.gates import CircuitGate
 
 _logger = logging.getLogger(__name__)
+
+
+def gate_count(block: Circuit) -> dict[Gate, int]:
+    return block.gate_counts
+
 
 class BlockAnalysisPass(BasePass):
     """
     Scaffold to analyze blocks of type CircuitGate.
 
-    A Circuit may be partitioned into blocks, each of which may have very 
-    different properties. This pass allows for blocks in a circuit to be 
+    A Circuit may be partitioned into blocks, each of which may have very
+    different properties. This pass allows for blocks in a circuit to be
     analyzed and compared quickly.
 
     This pass scans through a circuit, and stores the results of the scan in a
-    list variable called `results`. 
+    list variable called `results`.
     """
-
-    def gate_count(block : Circuit) -> dict[Gate, int]:
-        return block.gate_counts
 
     def __init__(
         self,
-        analysis_function: callable = gate_count,
-        filter_function: callable = lambda x: x,
+        analysis_function: Callable[[Circuit], Any] = gate_count,
+        filter_function: Callable[[Circuit], Any] = lambda x: x,
     ):
         """
         Construct a BlockAnalysisPass.
 
         Args:
             analysis_function (callable): A function to call on each block of
-                type `CircuitGate` in a circuit. Note that this function 
-                assumes the argument is given as a `Circuit`. 
+                type `CircuitGate` in a circuit. Note that this function
+                assumes the argument is given as a `Circuit`.
                 (Default: Circuit.gate_counts)
-            
+
             filter_function (callable | None): A function that will filter out
                 blocks that meet some criteron. If `filter_function` returns
                 False for some block, it will be filtered out of the analysis.
@@ -48,7 +51,7 @@ class BlockAnalysisPass(BasePass):
         """
         self.analysis_function = analysis_function
         self.filter_function = filter_function
-        self.results = []
+        self.results: list[Any] = []
 
     def run(self, circuit: Circuit, data: dict[str, Any] = {}) -> None:
         """Perform the pass's operation, see :class:`BasePass` for more."""
@@ -64,9 +67,9 @@ class BlockAnalysisPass(BasePass):
             # Convert to subcircuit
             block = Circuit(op.num_qudits)
             block.append_gate(
-                op.gate, 
-                [_ for _ in range(op.num_qudits)], 
-                op.params
+                op.gate,
+                [_ for _ in range(op.num_qudits)],
+                op.params,
             )
 
             block.unfold_all()
@@ -75,4 +78,3 @@ class BlockAnalysisPass(BasePass):
 
             result = self.analysis_function(block)
             self.results.append(result)
-        
