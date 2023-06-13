fixed gradient

ir/gates/qutrit/constant/h.py
@@ -1,13 +1,12 @@
 """This module implements the HGate."""
 from __future__ import annotations
 
-
-import numpy as np
+import math
 
 from bqskit.ir.gates.constantgate import ConstantGate
 from bqskit.ir.gates.qutritgate import QutritGate
 from bqskit.qis.unitary.unitarymatrix import UnitaryMatrix
-
+import numpy as np
 
 class HGate(ConstantGate, QutritGate):
     """
@@ -25,11 +24,8 @@ class HGate(ConstantGate, QutritGate):
 
     _num_qudits = 1
     _qasm_name = 'h'
-    _w = np.exp(2 * np.pi * 1j / 3)
+    _w=np.exp(2*np.pi*1j/3)
     _utry = UnitaryMatrix(
-        1 / np.sqrt(3) * np.array([
-            [1, 1, 1],
-            [1, _w, _w**2],
-            [1, _w**2, _w],
-        ]), [3],
-    )
+   1/np.sqrt(3)*np.array([[1,1,1],
+    [1,_w,_w**2],
+    [1,_w**2,_w]]),[3])

ir/gates/qutrit/parameterized/crx.py
@@ -3,82 +3,44 @@ from __future__ import annotations
 
 import numpy as np
 import numpy.typing as npt
+import jax
+jax.config.update("jax_enable_x64", True)
 
-from bqskit.ir.gates.qutrit import jaxcgate
-from bqskit.ir.gates.qutrit.constant.x import X01Gate
-from bqskit.ir.gates.qutrit.constant.x import X02Gate
-from bqskit.ir.gates.qutrit.constant.x import X12Gate
-from bqskit.ir.gates.qutrit.constant.x import XGate
 from bqskit.ir.gates.qutritgate import QutritGate
 from bqskit.qis.unitary.differentiable import DifferentiableUnitary
 from bqskit.qis.unitary.unitary import RealVector
 from bqskit.qis.unitary.unitarymatrix import UnitaryMatrix
 from bqskit.utils.cachedclass import CachedClass
-
-
-class CRXGate(
-    QutritGate,
-    DifferentiableUnitary,
-    CachedClass,
-):
-    """A gate representing a controlled X rotation for qutrits."""
-
-    _num_qudits = 2
-    _num_params = 1
-    _qasm_name = 'crx'
-
-    x = XGate._utry
-
-    def _unitary(self, params):
-        return jaxcgate(params[0], jax.scipy.linalg.expm(-1j * params[1] * x))
-
-    def _unitary_diff(self, params):
-        return jax.numpy.kron(pdict[params[0]], jax.scipy.linalg.expm(-1j * params[1] * x))
-
-    def get_unitary(self, params):
-        """Return the unitary for this gate, see :class:`Unitary` for more."""
-        return self._unitary(params).tolist()
-
-    def get_grad(self, params) -> npt.NDArray[np.complex128]:
-        """
-        Return the gradient for this gate.
-
-        See :class:`DifferentiableUnitary` for more info.
-        """
-        def _temp(param):
-            return self._unitary_diff([params[0], param])
-
-        return np.array(
-            [
-                np.zeros((9, 9)),
-                   np.array(jax.jacfwd(_temp)(params[1])),
-            ], dtype=np.complex128,
-        )
-
-
+from bqskit.ir.gates.qutrit.constant.x import XGate, X01Gate, X02Gate, X12Gate
+from bqskit.ir.gates.qutrit import jaxcgate, pdict
 class CRX01Gate(
     QutritGate,
     DifferentiableUnitary,
     CachedClass,
 ):
-    """A gate representing a controlled X rotation for qutrits."""
+    """
+    A gate representing a controlled X rotation for qutrits
+
+    
+    """
 
     _num_qudits = 2
     _num_params = 1
     _qasm_name = 'crx01'
 
-    x = X01Gate._utry
-
+    x = np.array(X01Gate._utry)
+    
     def _unitary(self, params):
-        return jaxcgate(params[0], jax.scipy.linalg.expm(-1j * params[1] * x))
-
+        return jaxcgate(params[0],jax.scipy.linalg.expm(-1j*params[1]*self.x))
+    
     def _unitary_diff(self, params):
-        return jax.numpy.kron(pdict[params[0]], jax.scipy.linalg.expm(-1j * params[1] * x))
-
+        return jax.numpy.kron(pdict[params[0]],jax.scipy.linalg.expm(-1j*params[1]*self.x))
+    
     def get_unitary(self, params):
         """Return the unitary for this gate, see :class:`Unitary` for more."""
-        return self._unitary(params).tolist()
-
+        return self._unitary(params)
+            
+       
     def get_grad(self, params) -> npt.NDArray[np.complex128]:
         """
         Return the gradient for this gate.
@@ -86,39 +48,42 @@ class CRX01Gate(
         See :class:`DifferentiableUnitary` for more info.
         """
         def _temp(param):
-            return self._unitary_diff([params[0], param])
-
+            return self._unitary_diff([params[0],param])
+      
         return np.array(
-            [
-                np.zeros((9, 9)),
-                   np.array(jax.jacfwd(_temp)(params[1])),
+            [ np.zeros((9,9)),
+                np.array(jax.jacfwd(_temp)(params[1])),
             ], dtype=np.complex128,
         )
-
-
+    
 class CRX02Gate(
     QutritGate,
     DifferentiableUnitary,
     CachedClass,
 ):
-    """A gate representing a controlled X rotation for qutrits."""
+    """
+    A gate representing a controlled X rotation for qutrits
+
+    
+    """
 
     _num_qudits = 2
     _num_params = 1
     _qasm_name = 'crx02'
 
-    x = X02Gate._utry
-
+    x = np.array(X02Gate._utry)
+    
     def _unitary(self, params):
-        return jaxcgate(params[0], jax.scipy.linalg.expm(-1j * params[1] * x))
-
+        return jaxcgate(params[0],jax.scipy.linalg.expm(-1j*params[1]*self.x))
+    
     def _unitary_diff(self, params):
-        return jax.numpy.kron(pdict[params[0]], jax.scipy.linalg.expm(-1j * params[1] * x))
-
+        return jax.numpy.kron(pdict[params[0]],jax.scipy.linalg.expm(-1j*params[1]*self.x))
+    
     def get_unitary(self, params):
         """Return the unitary for this gate, see :class:`Unitary` for more."""
-        return self._unitary(params).tolist()
-
+        return self._unitary(params)
+            
+       
     def get_grad(self, params) -> npt.NDArray[np.complex128]:
         """
         Return the gradient for this gate.
@@ -126,39 +91,42 @@ class CRX02Gate(
         See :class:`DifferentiableUnitary` for more info.
         """
         def _temp(param):
-            return self._unitary_diff([params[0], param])
-
+            return self._unitary_diff([params[0],param])
+      
         return np.array(
-            [
-                np.zeros((9, 9)),
-                   np.array(jax.jacfwd(_temp)(params[1])),
+            [ np.zeros((9,9)),
+                np.array(jax.jacfwd(_temp)(params[1])),
             ], dtype=np.complex128,
         )
-
-
+    
 class CRX12Gate(
     QutritGate,
     DifferentiableUnitary,
     CachedClass,
 ):
-    """A gate representing a controlled X rotation for qutrits."""
+    """
+    A gate representing a controlled X rotation for qutrits
+
+    
+    """
 
     _num_qudits = 2
     _num_params = 1
     _qasm_name = 'crx12'
 
-    x = X12Gate._utry
-
+    x = np.array(X12Gate._utry)
+    
     def _unitary(self, params):
-        return jaxcgate(params[0], jax.scipy.linalg.expm(-1j * params[1] * x))
-
+        return jaxcgate(params[0],jax.scipy.linalg.expm(-1j*params[1]*self.x))
+    
     def _unitary_diff(self, params):
-        return jax.numpy.kron(pdict[params[0]], jax.scipy.linalg.expm(-1j * params[1] * x))
-
+        return jax.numpy.kron(pdict[params[0]],jax.scipy.linalg.expm(-1j*params[1]*self.x))
+    
     def get_unitary(self, params):
         """Return the unitary for this gate, see :class:`Unitary` for more."""
-        return self._unitary(params).tolist()
-
+        return self._unitary(params)
+            
+       
     def get_grad(self, params) -> npt.NDArray[np.complex128]:
         """
         Return the gradient for this gate.
@@ -166,11 +134,10 @@ class CRX12Gate(
         See :class:`DifferentiableUnitary` for more info.
         """
         def _temp(param):
-            return self._unitary_diff([params[0], param])
-
+            return self._unitary_diff([params[0],param])
+      
         return np.array(
-            [
-                np.zeros((9, 9)),
-                   np.array(jax.jacfwd(_temp)(params[1])),
+            [ np.zeros((9,9)),
+                np.array(jax.jacfwd(_temp)(params[1])),
             ], dtype=np.complex128,
         )

ir/gates/qutrit/parameterized/crz.py
@@ -3,76 +3,41 @@ from __future__ import annotations
 
 import numpy as np
 import numpy.typing as npt
+import jax
+jax.config.update("jax_enable_x64", True)
+
 
 from bqskit.ir.gates.qubitgate import QubitGate
-from bqskit.ir.gates.qutrit import jaxcgate
-from bqskit.ir.gates.qutrit.constant.z import Z0Gate
-from bqskit.ir.gates.qutrit.constant.z import Z1Gate
-from bqskit.ir.gates.qutrit.constant.z import Z2Gate
-from bqskit.ir.gates.qutrit.constant.z import ZGate
 from bqskit.qis.unitary.differentiable import DifferentiableUnitary
 from bqskit.qis.unitary.unitary import RealVector
 from bqskit.qis.unitary.unitarymatrix import UnitaryMatrix
 from bqskit.utils.cachedclass import CachedClass
-
-
-class CRZGate():
-    """A gate representing a controlled rotation around the z-axis for
-    qutrits."""
-
-    _num_qudits = 2
-    _num_params = 2
-    _qasm_name = 'crz'
-
-    z = ZGate._utry
-
-    def _unitary(self, params):
-        return jaxcgate(params[0], jax.scipy.linalg.expm(-1j * params[1] * z))
-
-    def _unitary_diff(self, params):
-        return jax.numpy.kron(pdict[params[0]], jax.scipy.linalg.expm(-1j * params[1] * z))
-
-    def get_unitary(self, params):
-        """Return the unitary for this gate, see :class:`Unitary` for more."""
-        return self._unitary(params).tolist()
-
-    def get_grad(self, params) -> npt.NDArray[np.complex128]:
-        """
-        Return the gradient for this gate.
-
-        See :class:`DifferentiableUnitary` for more info.
-        """
-        def _temp(param):
-            return self._unitary_diff([params[0], param])
-
-        return np.array(
-            [
-                np.zeros((9, 9)),
-                   np.array(jax.jacfwd(_temp)(params[1])),
-            ], dtype=np.complex128,
-        )
-
+from bqskit.ir.gates.qutrit.constant.z import ZGate, Z0Gate, Z1Gate, Z2Gate
+from bqskit.ir.gates.qutrit import jaxcgate, pdict
 
 class CRZ0Gate():
-    """A gate representing a controlled rotation around the z-axis for
-    qutrits."""
+    """
+    A gate representing a controlled rotation around the z-axis for qutrits
+
+    """
 
     _num_qudits = 2
     _num_params = 2
     _qasm_name = 'crz0'
 
-    z = Z0Gate._utry
-
+    z = np.array(Z0Gate._utry)
+    
     def _unitary(self, params):
-        return jaxcgate(params[0], jax.scipy.linalg.expm(-1j * params[1] * z))
-
+        return jaxcgate(params[0],jax.scipy.linalg.expm(-1j*params[1]*self.z))
+    
     def _unitary_diff(self, params):
-        return jax.numpy.kron(pdict[params[0]], jax.scipy.linalg.expm(-1j * params[1] * z))
-
+        return jax.numpy.kron(pdict[params[0]],jax.scipy.linalg.expm(-1j*params[1]*self.z))
+    
     def get_unitary(self, params):
         """Return the unitary for this gate, see :class:`Unitary` for more."""
-        return self._unitary(params).tolist()
-
+        return self._unitary(params)
+            
+       
     def get_grad(self, params) -> npt.NDArray[np.complex128]:
         """
         Return the gradient for this gate.
@@ -80,36 +45,37 @@ class CRZ0Gate():
         See :class:`DifferentiableUnitary` for more info.
         """
         def _temp(param):
-            return self._unitary_diff([params[0], param])
-
+            return self._unitary_diff([params[0],param])
+      
         return np.array(
-            [
-                np.zeros((9, 9)),
-                   np.array(jax.jacfwd(_temp)(params[1])),
+            [ np.zeros((9,9)),
+                np.array(jax.jacfwd(_temp)(params[1])),
             ], dtype=np.complex128,
         )
 
-
 class CRZ1Gate():
-    """A gate representing a controlled rotation around the z-axis for
-    qutrits."""
+    """
+    A gate representing a controlled rotation around the z-axis for qutrits
+
+    """
 
     _num_qudits = 2
     _num_params = 2
     _qasm_name = 'crz1'
 
-    z = Z1Gate._utry
-
+    z = np.array(Z1Gate._utry)
+    
     def _unitary(self, params):
-        return jaxcgate(params[0], jax.scipy.linalg.expm(-1j * params[1] * z))
-
+        return jaxcgate(params[0],jax.scipy.linalg.expm(-1j*params[1]*self.z))
+    
     def _unitary_diff(self, params):
-        return jax.numpy.kron(pdict[params[0]], jax.scipy.linalg.expm(-1j * params[1] * z))
-
+        return jax.numpy.kron(pdict[params[0]],jax.scipy.linalg.expm(-1j*params[1]*self.z))
+    
     def get_unitary(self, params):
         """Return the unitary for this gate, see :class:`Unitary` for more."""
-        return self._unitary(params).tolist()
-
+        return self._unitary(params)
+            
+       
     def get_grad(self, params) -> npt.NDArray[np.complex128]:
         """
         Return the gradient for this gate.
@@ -117,36 +83,38 @@ class CRZ1Gate():
         See :class:`DifferentiableUnitary` for more info.
         """
         def _temp(param):
-            return self._unitary_diff([params[0], param])
-
+            return self._unitary_diff([params[0],param])
+      
         return np.array(
-            [
-                np.zeros((9, 9)),
-                   np.array(jax.jacfwd(_temp)(params[1])),
+            [ np.zeros((9,9)),
+                np.array(jax.jacfwd(_temp)(params[1])),
             ], dtype=np.complex128,
         )
-
-
+    
+    
 class CRZ2Gate():
-    """A gate representing a controlled rotation around the z-axis for
-    qutrits."""
+    """
+    A gate representing a controlled rotation around the z-axis for qutrits
+
+    """
 
     _num_qudits = 2
     _num_params = 2
     _qasm_name = 'crz2'
 
-    z = Z2Gate._utry
-
+    z = np.array(Z2Gate._utry)
+    
     def _unitary(self, params):
-        return jaxcgate(params[0], jax.scipy.linalg.expm(-1j * params[1] * z))
-
+        return jaxcgate(params[0],jax.scipy.linalg.expm(-1j*params[1]*self.z))
+    
     def _unitary_diff(self, params):
-        return jax.numpy.kron(pdict[params[0]], jax.scipy.linalg.expm(-1j * params[1] * z))
-
+        return jax.numpy.kron(pdict[params[0]],jax.scipy.linalg.expm(-1j*params[1]*self.z))
+    
     def get_unitary(self, params):
         """Return the unitary for this gate, see :class:`Unitary` for more."""
-        return self._unitary(params).tolist()
-
+        return self._unitary(params)
+            
+       
     def get_grad(self, params) -> npt.NDArray[np.complex128]:
         """
         Return the gradient for this gate.
@@ -154,11 +122,10 @@ class CRZ2Gate():
         See :class:`DifferentiableUnitary` for more info.
         """
         def _temp(param):
-            return self._unitary_diff([params[0], param])
-
+            return self._unitary_diff([params[0],param])
+      
         return np.array(
-            [
-                np.zeros((9, 9)),
-                   np.array(jax.jacfwd(_temp)(params[1])),
+            [ np.zeros((9,9)),
+                np.array(jax.jacfwd(_temp)(params[1])),
             ], dtype=np.complex128,
         )

ir/gates/qutrit/parameterized/rz.py
@@ -1,72 +1,39 @@
 """This module implements the RZGate."""
 from __future__ import annotations
 
-import jax
 import numpy as np
 import numpy.typing as npt
+import jax
+jax.config.update("jax_enable_x64", True)
 
-from bqskit.ir.gates.qutrit.constant.z import Z0Gate
-from bqskit.ir.gates.qutrit.constant.z import Z1Gate
-from bqskit.ir.gates.qutrit.constant.z import Z2Gate
-from bqskit.ir.gates.qutrit.constant.z import ZGate
 from bqskit.ir.gates.qutritgate import QutritGate
 from bqskit.qis.unitary.differentiable import DifferentiableUnitary
 from bqskit.qis.unitary.unitary import RealVector
 from bqskit.qis.unitary.unitarymatrix import UnitaryMatrix
 from bqskit.utils.cachedclass import CachedClass
-
-
-class RZGate(QutritGate, DifferentiableUnitary, CachedClass):
-    """A gate representing an arbitrary rotation around the Z axis for
-    qutrits."""
-
-    _num_qudits = 1
-    _num_params = 1
-    _qasm_name = 'rz'
-
-    z = ZGate._utry
-
-    def _unitary(self, params: RealVector = []):
-        return jax.scipy.linalg.expm(-1j * params[0] * z)
-
-    def get_unitary(self, params: RealVector = []) -> UnitaryMatrix:
-        """Return the unitary for this gate, see :class:`Unitary` for more."""
-        self.check_parameters(params)
-        return UnitaryMatrix(self._unitary(params).tolist())
-
-    def get_grad(self, params: RealVector = []) -> npt.NDArray[np.complex128]:
-        """
-        Return the gradient for this gate.
-
-        See :class:`DifferentiableUnitary` for more info.
-        """
-        self.check_parameters(params)
-
-        return np.array(
-            [
-                np.array(jax.jacfwd(self._unitary)(params)),
-            ], dtype=np.complex128,
-        )
-
+from bqskit.ir.gates.qutrit.constant.z import Z0Gate, Z1Gate, Z2Gate
 
 class RZ0Gate(QutritGate, DifferentiableUnitary, CachedClass):
-    """A gate representing an arbitrary rotation around the Z axis for
-    qutrits."""
+    """
+    A gate representing an arbitrary rotation around the Z axis for qutrits
+
+    """
 
     _num_qudits = 1
     _num_params = 1
     _qasm_name = 'rz0'
 
-    z = ZGate._utry
-
+    z = np.array(Z0Gate._utry)
+    
     def _unitary(self, params: RealVector = []):
-        return jax.scipy.linalg.expm(-1j * params[0] * z)
-
+        return jax.scipy.linalg.expm(-1j*params[0]*self.z)
+    
     def get_unitary(self, params: RealVector = []) -> UnitaryMatrix:
         """Return the unitary for this gate, see :class:`Unitary` for more."""
         self.check_parameters(params)
-        return UnitaryMatrix(self._unitary(params).tolist())
-
+        return UnitaryMatrix(self._unitary(params))
+            
+       
     def get_grad(self, params: RealVector = []) -> npt.NDArray[np.complex128]:
         """
         Return the gradient for this gate.
@@ -75,31 +42,34 @@ class RZ0Gate(QutritGate, DifferentiableUnitary, CachedClass):
         """
         self.check_parameters(params)
 
+        
         return np.array(
             [
                 np.array(jax.jacfwd(self._unitary)(params)),
             ], dtype=np.complex128,
         )
 
-
 class RZ1Gate(QutritGate, DifferentiableUnitary, CachedClass):
-    """A gate representing an arbitrary rotation around the Z axis for
-    qutrits."""
+    """
+    A gate representing an arbitrary rotation around the Z axis for qutrits
+
+    """
 
     _num_qudits = 1
     _num_params = 1
     _qasm_name = 'rz1'
 
-    z = Z1Gate._utry
-
+    z = np.array(Z1Gate._utry)
+    
     def _unitary(self, params: RealVector = []):
-        return jax.scipy.linalg.expm(-1j * params[0] * z)
-
+        return jax.scipy.linalg.expm(-1j*params[0]*self.z)
+    
     def get_unitary(self, params: RealVector = []) -> UnitaryMatrix:
         """Return the unitary for this gate, see :class:`Unitary` for more."""
         self.check_parameters(params)
-        return UnitaryMatrix(self._unitary(params).tolist())
-
+        return UnitaryMatrix(self._unitary(params))
+            
+       
     def get_grad(self, params: RealVector = []) -> npt.NDArray[np.complex128]:
         """
         Return the gradient for this gate.
@@ -108,31 +78,35 @@ class RZ1Gate(QutritGate, DifferentiableUnitary, CachedClass):
         """
         self.check_parameters(params)
 
+        
         return np.array(
             [
                 np.array(jax.jacfwd(self._unitary)(params)),
             ], dtype=np.complex128,
         )
-
-
+        
+        
 class RZ2Gate(QutritGate, DifferentiableUnitary, CachedClass):
-    """A gate representing an arbitrary rotation around the Z axis for
-    qutrits."""
+    """
+    A gate representing an arbitrary rotation around the Z axis for qutrits
+
+    """
 
     _num_qudits = 1
     _num_params = 1
     _qasm_name = 'rz2'
 
-    z = Z2Gate._utry
-
+    z = np.array(Z2Gate._utry)
+    
     def _unitary(self, params: RealVector = []):
-        return jax.scipy.linalg.expm(-1j * params[0] * z)
-
+        return jax.scipy.linalg.expm(-1j*params[0]*self.z)
+    
     def get_unitary(self, params: RealVector = []) -> UnitaryMatrix:
         """Return the unitary for this gate, see :class:`Unitary` for more."""
         self.check_parameters(params)
-        return UnitaryMatrix(self._unitary(params).tolist())
-
+        return UnitaryMatrix(self._unitary(params))
+           
+       
     def get_grad(self, params: RealVector = []) -> npt.NDArray[np.complex128]:
         """
         Return the gradient for this gate.
@@ -141,6 +115,7 @@ class RZ2Gate(QutritGate, DifferentiableUnitary, CachedClass):
         """
         self.check_parameters(params)
 
+        
         return np.array(
             [
                 np.array(jax.jacfwd(self._unitary)(params)),