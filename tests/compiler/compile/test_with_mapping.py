99f8cde96ea6504c22ad2b0c63191504b20a3081
Ed Younis <edyounis123@gmail.com>
2023-08-17 13:00:58 -0400

    Updated test

--- tests/compiler/compile/test_with_mapping.py
+++ tests/compiler/compile/test_with_mapping.py
@@ -61,4 +61,4 @@ def test_compile_with_mapping(
     out_utry = mapped_subcircuit.get_unitary()
     PI = PermutationMatrix.from_qubit_location(3, local_qudit_initial_perm)
     PF = PermutationMatrix.from_qubit_location(3, local_qudit_final_perm)
-    assert out_utry.get_distance_from(PF.T @ correct_utry @ PI) < 1e-7
+    assert out_utry.get_distance_from(PF.T @ correct_utry @ PI) < 1e-6
