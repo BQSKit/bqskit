cb144926c4429e730e9feebf77af97c759884749
Mathias Weiden <mtweiden@berkeley.edu>
2023-08-30 18:52:26 -0700

    formatting

--- tests/passes/util/test_structure.py
+++ tests/passes/util/test_structure.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from bqskit.compiler.compiler import Compiler
 from bqskit.ir.circuit import Circuit
-from bqskit.ir.structure import CircuitStructure
 from bqskit.ir.gates.constant.cx import CNOTGate
+from bqskit.ir.structure import CircuitStructure
 from bqskit.passes import QuickPartitioner
 from bqskit.passes.util.structure import StructureAnalysisPass
 
@@ -18,7 +18,7 @@ def test_list_structures(compiler: Compiler) -> None:
     num_blocks = 3
     for block_num in range(num_blocks):
         block = Circuit(2)
-        for _ in range(block_num+1):
+        for _ in range(block_num + 1):
             block.append_gate(CNOTGate(), (0, 1))
         structure = CircuitStructure(block)
         true_structures.append(structure)
