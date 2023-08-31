e6ca748867423a676746f653a9f7fc3aa32d9b49
Mathias Weiden <mtweiden@berkeley.edu>
2023-08-30 18:08:29 -0700

    created structure analysis pass

---
+++ bqskit/passes/util/structure.py
@@ -0,0 +1,33 @@
+"""This module implements the StructureAnalysisPass."""
+from __future__ import annotations
+
+from bqskit.compiler.basepass import BasePass
+from bqskit.compiler.passdata import PassData
+from bqskit.ir.circuit import Circuit
+from bqskit.ir.circuit import CircuitGate
+
+
+class StructureAnalysisPass(BasePass):
+    """A pass that catalogs circuit structures used in a partitioned circ-
+    uit."""
+
+    def __init__(self) -> None:
+        """Construct a StructureAnalysisPass."""
+
+    async def run(self, circuit: Circuit, data: PassData) -> None:
+        """Perform the pass's operation, see :class:`BasePass` for more."""
+        if 'structures' not in data:
+            data['structures'] = []
+
+        structures_seen = []
+
+        for block in circuit:
+            if not isinstance(block.gate, CircuitGate):
+                continue
+            subcirc = Circuit.from_operation(block)
+            subcirc.unfold_all()
+            subcirc.set_params([0] * subcirc.num_params)
+
+            structures_seen.append(subcirc)
+
+        data['structures'].extend(structures_seen)
