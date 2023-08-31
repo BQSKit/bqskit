387e19a5f0d0a6d3f3d1298a6e0dec7a52308901
Mathias Weiden <mtweiden@berkeley.edu>
2023-08-30 18:26:17 -0700

    updated structure test

--- tests/passes/util/test_structure.py
+++ tests/passes/util/test_structure.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from bqskit.compiler.compiler import Compiler
 from bqskit.ir.circuit import Circuit
+from bqskit.ir.structure import CircuitStructure
 from bqskit.ir.gates.constant.cx import CNOTGate
 from bqskit.passes import QuickPartitioner
 from bqskit.passes.util.structure import StructureAnalysisPass
@@ -12,15 +13,24 @@ def test_list_structures(compiler: Compiler) -> None:
     partitioner = QuickPartitioner(2)
     analyzer = StructureAnalysisPass()
     circuit = Circuit(3)
+
+    true_structures = []
+    num_blocks = 3
+    for block_num in range(num_blocks):
+        block = Circuit(2)
+        for _ in range(block_num+1):
+            block.append_gate(CNOTGate(), (0, 1))
+        structure = CircuitStructure(block)
+        true_structures.append(structure)
+
     for edge in [(0, 1), (1, 2), (1, 2), (0, 2), (0, 2), (0, 2)]:
         circuit.append_gate(CNOTGate(), edge)
-
     _, data = compiler.compile(
         circuit, [partitioner, analyzer], request_data=True,
     )
 
-    structures = data['structures']
-    assert len(structures) == 3
-    assert structures[0].depth == 1
-    assert structures[1].depth == 2
-    assert structures[2].depth == 3
+    pass_structures = data['structures']
+    assert len(pass_structures) == 3
+    assert pass_structures[0] == true_structures[0]
+    assert pass_structures[1] == true_structures[1]
+    assert pass_structures[2] == true_structures[2]
