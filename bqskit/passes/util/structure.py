09ffa1ba5f4b58837f97bc88538aa15d316e1d66
Mathias Weiden <mtweiden@berkeley.edu>
2023-08-30 18:13:29 -0700

    structure analysis pass uses Structure

--- bqskit/passes/util/structure.py
+++ bqskit/passes/util/structure.py
@@ -5,29 +5,37 @@ from bqskit.compiler.basepass import BasePass
 from bqskit.compiler.passdata import PassData
 from bqskit.ir.circuit import Circuit
 from bqskit.ir.circuit import CircuitGate
+from bqskit.ir.structure import CircuitStructure
 
 
 class StructureAnalysisPass(BasePass):
-    """A pass that catalogs circuit structures used in a partitioned circ-
-    uit."""
+    """
+    A pass that catalogs circuit structures used in a partitioned circuit.
+
+    Note: For each partitioned CircuitGate in the Circuit, the recursive
+    `unfold_all` method is called to ensure that structure is defined at
+    the gate level. For structures to be considered the same, they must
+    consist of the same gates, in the same locations.
+    """
 
     def __init__(self) -> None:
-        """Construct a StructureAnalysisPass."""
+        """Construct a StructurePass."""
 
     async def run(self, circuit: Circuit, data: PassData) -> None:
         """Perform the pass's operation, see :class:`BasePass` for more."""
+
         if 'structures' not in data:
             data['structures'] = []
 
-        structures_seen = []
+        structures_seen: list[CircuitStructure] = []
 
         for block in circuit:
             if not isinstance(block.gate, CircuitGate):
                 continue
             subcirc = Circuit.from_operation(block)
+            # Structure depends on the gate level, call unfold_all
             subcirc.unfold_all()
-            subcirc.set_params([0] * subcirc.num_params)
-
-            structures_seen.append(subcirc)
+            structure = CircuitStructure(subcirc)
+            structures_seen.append(structure)
 
         data['structures'].extend(structures_seen)
