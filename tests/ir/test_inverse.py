3e4f8a93ac252ed8868b56fe4bdea607ab80d94e
Ed Younis <edyounis123@gmail.com>
2023-09-04 22:44:26 -0400

    Updated test accuracy metric

--- tests/ir/test_inverse.py
+++ tests/ir/test_inverse.py
@@ -11,5 +11,6 @@ from bqskit.utils.test.strategies import operations
 def test_gate_inverse(op: Operation) -> None:
     inverse_op = op.get_inverse()
     iden = np.identity(op.dim)
-    dist = (inverse_op.get_unitary() @ op.get_unitary()).get_distance_from(iden)
-    assert dist < 1e-7
+    supposed_to_be_iden = (inverse_op.get_unitary() @ op.get_unitary())
+    dist = supposed_to_be_iden.get_distance_from(iden, 1)
+    assert dist < 1e-10
