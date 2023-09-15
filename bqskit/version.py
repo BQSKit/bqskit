527077f649a72550df4189926d519b59c58b093b
Ed Younis <edyounis123@gmail.com>
2023-09-15 07:43:38 -0400

    1.1.0

--- bqskit/version.py
+++ bqskit/version.py
@@ -1,4 +1,4 @@
 """This module contains the version information for BQSKit."""
 from __future__ import annotations
-__version_info__ = ('1', '1', '0', 'rc1')
+__version_info__ = ('1', '1', '0')
 __version__ = '.'.join(__version_info__[:3]) + ''.join(__version_info__[3:])
