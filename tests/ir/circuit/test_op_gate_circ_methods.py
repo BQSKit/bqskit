b94ce8f71c34cc2f54b27af71cef2e2eb3dcb632
Ed Younis <edyounis123@gmail.com>
2023-10-17 06:08:53 -0400

    Removed error case in append_circuit

--- tests/ir/circuit/test_op_gate_circ_methods.py
+++ tests/ir/circuit/test_op_gate_circ_methods.py
@@ -339,8 +339,6 @@ class TestAppendCircuit:
 
     @given(circuits())
     def test_reconstruct(self, circuit: Circuit) -> None:
-        if circuit.num_operations == 0:
-            return
         new_circuit = Circuit(circuit.num_qudits, circuit.radixes)
         new_circuit.append_circuit(circuit, list(range(circuit.num_qudits)))
         check_no_idle_cycles(new_circuit)
@@ -348,8 +346,6 @@ class TestAppendCircuit:
 
     @given(circuits())
     def test_reconstruct_larger(self, circuit: Circuit) -> None:
-        if circuit.num_operations == 0:
-            return
         new_circ = Circuit(circuit.num_qudits + 1, circuit.radixes + (2,))
         new_circ.append_circuit(circuit, list(range(circuit.num_qudits)))
         check_no_idle_cycles(new_circ)
