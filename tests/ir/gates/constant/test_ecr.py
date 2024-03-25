70e2a5477f7ddd5c39bb8e065b2f20c9bc47fa7a
Vincent Gierisch <vincentgierisch@gmail.com>
2024-03-25 02:39:01 +0100

    Add ECR test

---
+++ tests/ir/gates/constant/test_ecr.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from bqskit.ir.circuit import Circuit
+from bqskit.ir.gates import CXGate
+from bqskit.ir.gates import HGate
+from bqskit.ir.gates import RZGate
+from bqskit.ir.gates import XGate
+from bqskit.ir.gates import ECRGate
+import numpy as np
+
+def test_ecr() -> None:
+    c = Circuit(2)
+    c.append_gate(HGate(), 1)
+    c.append_gate(CXGate(), (0, 1))
+    c.append_gate(RZGate(np.pi/4.0), 1) 
+    c.append_gate(CXGate(), (0, 1))
+    c.append_gate(HGate(), 1)
+
+    c.append_gate(XGate(), 0)
+
+    c.append_gate(HGate(), 1)
+    c.append_gate(CXGate(), (0, 1))
+    c.append_gate(RZGate(-np.pi/4.0), 1) 
+    c.append_gate(CXGate(), (0, 1))
+    c.append_gate(HGate(), 1)
+
+
+    print(np.round(c.get_unitary(), 1))
+
+    assert c.get_unitary().get_distance_from(ECRGate().get_unitary()) < 3e-8
