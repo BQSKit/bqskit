31ec6b52f0e86e155975870b97f4d6a6d235fcab
Ed Younis <edyounis123@gmail.com>
2024-03-26 12:30:55 -0400

    Removed deprecated rebase pass

--- bqskit/passes/processing/__init__.py
+++ bqskit/passes/processing/__init__.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 from bqskit.passes.processing.exhaustive import ExhaustiveGateRemovalPass
 from bqskit.passes.processing.iterative import IterativeScanningGateRemovalPass
-from bqskit.passes.processing.rebase import Rebase2QuditGatePass  # TODO: Remove
 from bqskit.passes.processing.scan import ScanningGateRemovalPass
 from bqskit.passes.processing.substitute import SubstitutePass
 
@@ -12,5 +11,4 @@ __all__ = [
     'IterativeScanningGateRemovalPass',
     'ScanningGateRemovalPass',
     'SubstitutePass',
-    'Rebase2QuditGatePass',
 ]
