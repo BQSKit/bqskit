70059ffad7fa65b28f78bcbdcfc46c868cedbde1
Mathias Weiden <mtweiden@berkeley.edu>
2024-08-28 11:29:06 -0700

    Reodered imports

--- bqskit/passes/synthesis/__init__.py
+++ bqskit/passes/synthesis/__init__.py
@@ -1,6 +1,7 @@
 """This package implements synthesis passes and synthesis related classes."""
 from __future__ import annotations
 
+from bqskit.passes.synthesis.diagonal import WalshDiagonalSynthesisPass
 from bqskit.passes.synthesis.leap import LEAPSynthesisPass
 from bqskit.passes.synthesis.pas import PermutationAwareSynthesisPass
 from bqskit.passes.synthesis.qfast import QFASTDecompositionPass
@@ -8,7 +9,6 @@ from bqskit.passes.synthesis.qpredict import QPredictDecompositionPass
 from bqskit.passes.synthesis.qsearch import QSearchSynthesisPass
 from bqskit.passes.synthesis.synthesis import SynthesisPass
 from bqskit.passes.synthesis.target import SetTargetPass
-from bqskit.passes.synthesis.diagonal import WalshDiagonalSynthesisPass
 
 __all__ = [
     'LEAPSynthesisPass',
