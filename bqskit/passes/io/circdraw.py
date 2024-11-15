914790b319c89e35901d5bd1b66fddb8b5d7d023
user <>
2024-11-15 11:31:33 -0600

    Added CircuitDrawPass for intermediate visualization

---
+++ bqskit/passes/io/circdraw.py
@@ -0,0 +1,17 @@
+from bqskit.compiler import BasePass
+from bqskit.compiler.passdata import PassData
+from bqskit.ir.circuit import Circuit
+from bqskit.ext import bqskit_to_qiskit
+from qiskit import *
+
+class CircuitDrawPass(BasePass):
+
+    def __init__(
+        self,
+        marker: str = 'circuit',
+    ) -> None:
+        self.marker = marker
+
+    async def run(self, circuit: Circuit, data: PassData):
+        qc = bqskit_to_qiskit(circuit)
+        qc.draw(output='mpl', filename=self.marker+'.png', fold=-1)
